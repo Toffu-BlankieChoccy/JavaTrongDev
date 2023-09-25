77beaf35d24042ff77a166905b83d890aff56533
Toffu-kum <70308128+Toffu-kum@users.noreply.github.com>
2023-09-25 17:37:04 +0700

    Ex6

--- Exercise6/app/src/main/java/com/example/exercise6/MainActivity.java
+++ Exercise6/app/src/main/java/com/example/exercise6/MainActivity.java
@@ -3,11 +3,16 @@ package com.example.exercise6;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.animation.AnimatableView;
+
+//Implement click event
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     Button addBtn, minusBtn, multiBtn, divBtn;
     EditText num1, num2;
     TextView textView;
@@ -24,6 +29,37 @@ public class MainActivity extends AppCompatActivity {
         num1 = findViewById(R.id.num1);
         num2 = findViewById(R.id.num2);
         textView = findViewById(R.id.result);
+
+        addBtn.setOnClickListener(this);minusBtn.setOnClickListener(this);multiBtn.setOnClickListener(this);divBtn.setOnClickListener(this);
+    }
+
+    public int getNum(EditText editText)
+    {
+        if(editText.getText().toString().equals("")) {
+            Toast.makeText(this, "Number(s) is required to be filled", Toast.LENGTH_SHORT).show();
+            return 0;
+        }
+        return Integer.parseInt(editText.getText().toString());
+    }
+
+    @Override
+    public void onClick(View view) {
+        n1 = getNum(num1);
+        n2 = getNum(num2);
+        int id = view.getId();
+        if (id == R.id.addBtn) {
+            textView.setText(n1 + " + " + n2 + " = " + (n1 + n2));
+        } else if (id == R.id.minusBtn) {
+            textView.setText(n1 + " - " + n2 + " = " + (n1 - n2));
+        } else if (id == R.id.multiBtn) {
+            textView.setText(n1 + " x " + n2 + " = " + (n1 * n2));
+        } else if (id == R.id.divBtn) {
+            if (n2 == 0){
+                Toast.makeText(this, "Cannot devide by 0", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            textView.setText(n1 + " / " + n2 + " = " + (n1 / n2));
+        }
 
     }
 }
