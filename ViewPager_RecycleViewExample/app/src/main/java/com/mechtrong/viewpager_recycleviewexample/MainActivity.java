1000da69ec738a11850fbaf72b55d77e635c6a7b
Toffu-kum <trnhoangtrong@gmail.com>
2023-11-15 12:24:11 +0700

    Recycle Tabs

---
+++ ViewPager_RecycleViewExample/app/src/main/java/com/mechtrong/viewpager_recycleviewexample/MainActivity.java
@@ -0,0 +1,53 @@
+package com.mechtrong.viewpager_recycleviewexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
+
+import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    CountryRVAdapter countryRVAdapter;
+    ArrayList<Country> dsQG;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        dsQG = new ArrayList<Country>();
+        Country qg1 = new Country("Vietnam","vn",80);
+        Country qg2 = new Country("United State","us",68);
+        Country qg3 = new Country("Russia","ru",120);
+        dsQG.add(qg1);
+        dsQG.add(qg2);
+        dsQG.add(qg3);
+//
+//        RecyclerView recyclerView = (RecyclerView)findViewById(R.id.myrecycleView);
+//
+//        LinearLayoutManager llm = new LinearLayoutManager(this);
+//
+//        recyclerView.setLayoutManager(llm);
+////        recyclerView.addItemDecoration(new DividerItemDecoration(this,
+////                DividerItemDecoration.VERTICAL));
+//        DividerItemDecoration itemDecorator = new DividerItemDecoration(this, DividerItemDecoration.VERTICAL);
+//
+//        itemDecorator.setDrawable(ContextCompat.getDrawable(this, R.drawable.divider));
+//
+//        recyclerView.addItemDecoration(itemDecorator);
+//        countryRVAdapter = new CountryRVAdapter(dsQG);
+//        recyclerView.setAdapter(countryRVAdapter);
+//        countryRVAdapter.notifyDataSetChanged();
+        ViewPager2 viewPager2 = findViewById(R.id.viewPagerCountry);
+
+        CountryPageAdapter countryPageAdapter  = new CountryPageAdapter(this, dsQG);
+        viewPager2.setAdapter(countryPageAdapter);
+
+        TabLayout tabLayout = findViewById(R.id.tabLayout);
+
+        new TabLayoutMediator(tabLayout,viewPager2, (tab, position) -> tab.setText("Tab " + (position + 1))
+        ).attach();
+    }
+}
