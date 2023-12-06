d112dda0d65ca9987474856134eb7dea85b91590
Toffu-kum <trnhoangtrong@gmail.com>
2023-12-06 15:09:01 +0700

    Dự án chat online (doing)

--- AppOnlineChattingProject_63135901/app/src/main/java/com/mechtrong/apponlinechattingproject_63135901/Activities/SignInActivity.java
+++ AppOnlineChattingProject_63135901/app/src/main/java/com/mechtrong/apponlinechattingproject_63135901/Activities/SignInActivity.java
@@ -5,9 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
+import com.google.firebase.Firebase;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.mechtrong.apponlinechattingproject_63135901.R;
 import com.mechtrong.apponlinechattingproject_63135901.databinding.ActivitySignInBinding;
+
+import java.util.HashMap;
 
 public class SignInActivity extends AppCompatActivity {
 
@@ -23,7 +28,23 @@ public class SignInActivity extends AppCompatActivity {
     private void setListener(){
         binding.textCreateNewAccount.setOnClickListener(v ->
                 startActivity(new Intent(getApplicationContext(), SignUpActivity.class)));
+        binding.buttonSignIn.setOnClickListener(v -> addDataToFireStore());
     }
 
+    private void addDataToFireStore(){
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
+        HashMap<String, Object> data = new HashMap<>();
+        data.put("first_name", "Trong");
+        data.put("last_name", "Pete");
+        database.collection("users")
+                .add(data)
+                .addOnSuccessListener(documentReference -> {
+                    Toast.makeText(getApplicationContext(), "Data informed", Toast.LENGTH_LONG).show();
+                })
+                .addOnFailureListener(exception -> {
+                    Toast.makeText(getApplicationContext(), exception.getMessage(), Toast.LENGTH_LONG).show();
+                });
 
+
+    }
 }
--- AppOnlineChattingProject_63135901/app/src/main/java/com/mechtrong/apponlinechattingproject_63135901/FireBase/MessagingService.java
+++ AppOnlineChattingProject_63135901/app/src/main/java/com/mechtrong/apponlinechattingproject_63135901/FireBase/MessagingService.java
@@ -11,16 +11,10 @@ public class MessagingService extends FirebaseMessagingService {
     @Override
     public void onNewToken(@NonNull String token) {
         super.onNewToken(token);
-
-        //Test Token
-        Log.d("FCM", "Token: " + token );
     }
 
     @Override
     public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
         super.onMessageReceived(remoteMessage);
-
-        //Test Remote message
-        Log.d("FCM", "Remote Message" + remoteMessage.getNotification().getBody());
     }
 }
