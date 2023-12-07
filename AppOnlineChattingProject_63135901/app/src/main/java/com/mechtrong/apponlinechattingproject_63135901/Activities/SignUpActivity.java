e96dd3ad4646e0e9109b2738ba5f286e5095bae1
Toffu-kum <trnhoangtrong@gmail.com>
2023-12-07 21:05:48 +0700

    Dự án chat online (doing) (setting up SignUpActivity)

--- AppOnlineChattingProject_63135901/app/src/main/java/com/mechtrong/apponlinechattingproject_63135901/Activities/SignUpActivity.java
+++ AppOnlineChattingProject_63135901/app/src/main/java/com/mechtrong/apponlinechattingproject_63135901/Activities/SignUpActivity.java
@@ -1,36 +1,40 @@
 package com.mechtrong.apponlinechattingproject_63135901.Activities;
 
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContracts;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.PatternMatcher;
+import android.provider.MediaStore;
 import android.util.Base64;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Toast;
 
-import com.mechtrong.apponlinechattingproject_63135901.R;
-import com.mechtrong.apponlinechattingproject_63135901.databinding.ActivitySignInBinding;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.mechtrong.apponlinechattingproject_63135901.Utilities.Constants;
+import com.mechtrong.apponlinechattingproject_63135901.Utilities.PrefrencesManager;
 import com.mechtrong.apponlinechattingproject_63135901.databinding.ActivitySignUpBinding;
 
 import java.io.ByteArrayOutputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
+import java.util.HashMap;
 
 public class SignUpActivity extends AppCompatActivity {
     private ActivitySignUpBinding binding;
+    private PrefrencesManager prefrencesManager;
     private String encodeImage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivitySignUpBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        prefrencesManager = new PrefrencesManager(getApplicationContext());
         setListener();
     }
 
@@ -41,6 +45,11 @@ public class SignUpActivity extends AppCompatActivity {
                signUp();
            }
        });
+       binding.layoutImage.setOnClickListener(v -> {
+           Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+           intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+           pickImage.launch(intent);
+       });
     }
 
     private void showToast(String mess)
@@ -49,6 +58,30 @@ public class SignUpActivity extends AppCompatActivity {
     }
 
     private void signUp(){
+        loading(true);
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
+        HashMap<String, Object> user = new HashMap<>();
+        user.put(Constants.KEY_NAME,binding.inputName.getText().toString());
+        user.put(Constants.KEY_EMAIL,binding.inputEmail.getText().toString());
+        user.put(Constants.KEY_PASSWORD,binding.inputPassword.getText().toString());
+        user.put(Constants.KEY_IMAGE,encodeImage);
+        database.collection(Constants.KEY_COLLECTION_USERS).add(user)
+                .addOnSuccessListener(documentReference -> {
+                    loading(false);
+                    prefrencesManager.putBoolean(Constants.KEY_IS_SIGNED_IN, true);
+                    prefrencesManager.putString(Constants.KEY_USER_ID, documentReference.getId());
+                    prefrencesManager.putString(Constants.KEY_NAME, binding.inputName.getText().toString());
+                    prefrencesManager.putString(Constants.KEY_IMAGE, encodeImage);
+                    Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+                })
+                .addOnFailureListener(exception -> {
+                    loading(false);
+                    showToast(exception.getMessage());
+                });
+
+
 
     }
 
