d4939ca18fda98041aaeb34e114de84bb87fb287
Toffu-kum <trnhoangtrong@gmail.com>
2023-12-20 15:51:18 +0700

    Dự án chat online (doing) (Chat Layout)

---
+++ AppOnlineChattingProject_63135901/app/src/main/java/com/mechtrong/apponlinechattingproject_63135901/Activities/ChatActivity.java
@@ -0,0 +1,16 @@
+package com.mechtrong.apponlinechattingproject_63135901.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.mechtrong.apponlinechattingproject_63135901.R;
+
+public class ChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+    }
+}
